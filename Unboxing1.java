6692de8820a7f7ef5d956f3852124a18418cd261
DhanuSree731 <2420090101@klh.edu.in>
2025-07-22 15:31:18 +0530

    Add files via upload

---
+++ Unboxing1.java
@@ -0,0 +1,11 @@
+package open;
+
+public class Unboxing1 {
+public static void main(String[] args) {
+	Integer a=new Integer(3);
+	int i=a.intValue();
+	int j=a;
+	System.out.println(a+" "+i+" "+j);
+	
+}
+}
